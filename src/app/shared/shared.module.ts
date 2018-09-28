5b51f9bb895c6b3d4c1c647676129d8a347771fb
Igor Josifov <igority@gmail.com>
2018-09-28 16:18:16 -0400

    setting up structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule,
   ],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent]
 })
 export class SharedModule { }
